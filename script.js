4756655bf59336f798a0486ee9e42a297f5288da
Thor138 <tjcourey84@gmail.com>
2021-05-05 13:46:45 -0600

    added strage functions to script, added display function for local storage

--- script.js
+++ script.js
@@ -66,31 +66,69 @@ var availLang = [
   "German-accent",
   "Draconic",
 ];
+var beerInfo;
+var favBeer = [];
 
-function translate(requestUrl) {
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-}
-translate(langUrl(availLang, "coors"));
+// var subBut = $(".btn-primary");
+// var prevDis = $(".lower-inputs");
 
-function langUrl(arr, beer) {
-  let r = Math.floor(Math.random() * arr.length);
-  lang = arr[r];
-  console.log(lang);
+// function translate(requestUrl) {
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
+// }
+// translate(langUrl(availLang, "coors"));
 
-  langUrl = `https://api.funtranslations.com/translate/${lang}.json?text=Bartender,%20I%20would%20like%20to%20order%20a%20beer.%20Make%20it%20a%20${beer}`;
-  return langUrl;
-}
-fetch("https://https://api.punkapi.com/v2/beers?page=2&per_page=100")
+// function langUrl(arr, beer) {
+//   let r = Math.floor(Math.random() * arr.length);
+//   lang = arr[r];
+//   console.log(lang);
+
+//   langUrl = `https://api.funtranslations.com/translate/${lang}.json?text=Bartender,%20I%20would%20like%20to%20order%20a%20beer.%20Make%20it%20a%20${beer}`;
+//   return langUrl;
+// }
+fetch("https://api.punkapi.com/v2/beers?page=2")
   .then(function (response) {
     console.log(response);
     return response.json();
   })
   .then(function (data) {
     console.log(data);
+    beerInfo = data;
   });
+
+function storeFav(obj) {
+  var disArr = JSON.parse(localStorage.getItem("favBeer")) || [];
+  disArr.unshift(obj);
+  if (disArr.length > 3) {
+    disArr.pop();
+  }
+  localStorage.setItem("favBeer", JSON.stringify(disArr));
+}
+
+var displayHist = function () {
+  var disArr = JSON.parse(localStorage.getItem("favBeer")) || [];
+
+  $(".card-deck").each(function (i) {
+    $(this).append(
+      `<div class="card">
+        <div class="card-body">
+        <h3>Beer Name: ${disArr[i].name}</h3>
+        <p>${disArr[i].tagline}</p>
+        <p>${disArr[i].abv}</p>
+        </div>
+      </div>`
+    );
+  });
+};
+function displayResult(data) {
+  $("temp").text(data[0].image_url);
+  $("temp").text(data[0].tagline);
+  $("temp").text(data[0].description);
+  $("temp").text(data[0].abv);
+  $("temp").text(data[0].food_pairing[0]);
+}
